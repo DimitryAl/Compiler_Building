473cc331bb6a8a517323c5e4b18d2cf4e90bc74d
Dmitry Alexeev <dalexeev.work@gmail.com>
2022-02-23 17:46:04 +0300

    .

--- LaboratoryWork1/LaboratoryWork1.cpp
+++ LaboratoryWork1/LaboratoryWork1.cpp
@@ -7,10 +7,12 @@
 class StateMachine
 {
 private:
-	char _new_line = '/n';
+	//char _new_line = '/n';
 	std::vector<char> _alphabet{	// Входной алфавит
 		'0', '1', '2', '3', '4',
 		'5', '6', '7', '8', '9', (char)'/n'};
+	std::vector<char> _init_floors{};	// Очередь нажатий
+	std::vector<char> _final_floors{};	// Очередь конечных этажей
 	enum class State {    // Состояния автомата
 		Start,
 		Moving_Up,
@@ -19,10 +21,52 @@ private:
 		Error,
 		Final
 	};
-	State _current_state = State::Standing;
-	int _current_floor = 0;
-	int _final_floor = 0;
+	State _current_state = State::Start;	// Текущее состояние
+	State _finite_state = State::Final;		// Конечное состояние
+	int _current_floor = 0;	// Текущий этаж
+	int _final_floor = 0;	// Конечный этаж
+	
 
+	
+
+	
+public:
+	StateMachine() {};
+
+	void Transition(/*char symbol*/)
+	{
+		switch (_current_state)
+		{
+		case State::Start:
+			for (int i = 0; )
+			break;
+		case State::Moving_Up:
+
+			break;
+		case State::Moving_Down:
+
+			break;
+		case State::Standing:
+
+			break;
+		case State::Error:
+
+			break;
+		case State::Final:
+
+			break;
+		default:
+			break;
+		}
+	}
+	void AddQueue(int n, char symbol) {
+		if (n == 1) _init_floors.push_back(symbol);
+		else if (n == 2) _final_floors.push_back(symbol);
+	}
+	char GetData(std::ifstream& file)
+	{
+		return file.get();
+	};
 	bool InAlphabet(char inputSymbol) {
 		auto result = std::find(begin(_alphabet), end(_alphabet), inputSymbol);
 		if (result != end(_alphabet)) {
@@ -30,32 +74,14 @@ private:
 		}
 		return false;
 	}
-public:
-	StateMachine()
-	{
-		/*this->current_floor = 1;
-		this->final_floor = 1;*/
-	}
-	/*StateMachine(int floor)
-	{
-		this->_current_floor = floor;
-	}*/
-
-
-	char GetData(std::ifstream& file)
-	{
-		/*char symbol;
-		symbol = file.get();*/
-		return file.get();
-	};
 	int GetCurrentFloor()
 	{
 		return this->_current_floor;
 	}
-	int GetFinalFloor()
-	{
-		return this->_final_floor;
-	}
+	//int GetFinalFloor()
+	//{
+	//	return this->_final_floor;
+	//}
 	bool SetCurrentFloor(int floor) {
 		if (InAlphabet(floor)) { 
 			_current_floor = floor; 
@@ -63,13 +89,13 @@ public:
 		}
 		else return false;
 	}
-	bool SetFinalFloor(int floor) {
+	/*bool SetFinalFloor(int floor) {
 		if (InAlphabet(floor)) {
 			_final_floor = floor;  
 			return true;
 		}
 		else return false;
-	}
+	}*/
 
 };
 
@@ -82,11 +108,15 @@ int main()
 
 	StateMachine Elevator;
 
-	char symbol;
-	while (0)
+	char init_floor;
+	char final_floor;
+	while (!file.eof())
 	{
-		symbol = Elevator.GetData(file);
-
+		init_floor = Elevator.GetData(file);
+		final_floor = Elevator.GetData(file);
+		Elevator.AddQueue(1, init_floor);
+		Elevator.AddQueue(2, final_floor);
+		Elevator.Transition();
 	}
 
 
