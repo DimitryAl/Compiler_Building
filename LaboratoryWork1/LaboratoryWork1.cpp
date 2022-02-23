a99b3e45faa174df517133b66f040999c29253e1
Dmitry Alexeev <dalexeev.work@gmail.com>
2022-02-23 17:08:22 +0300

    .

--- LaboratoryWork1/LaboratoryWork1.cpp
+++ LaboratoryWork1/LaboratoryWork1.cpp
@@ -1,20 +1,96 @@
 // LaboratoryWork1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
+#include <fstream>
+#include <vector>
+
+class StateMachine
+{
+private:
+	char _new_line = '/n';
+	std::vector<char> _alphabet{	// Входной алфавит
+		'0', '1', '2', '3', '4',
+		'5', '6', '7', '8', '9', (char)'/n'};
+	enum class State {    // Состояния автомата
+		Start,
+		Moving_Up,
+		Moving_Down,
+		Standing,
+		Error,
+		Final
+	};
+	State _current_state = State::Standing;
+	int _current_floor = 0;
+	int _final_floor = 0;
+
+	bool InAlphabet(char inputSymbol) {
+		auto result = std::find(begin(_alphabet), end(_alphabet), inputSymbol);
+		if (result != end(_alphabet)) {
+			return true;
+		}
+		return false;
+	}
+public:
+	StateMachine()
+	{
+		/*this->current_floor = 1;
+		this->final_floor = 1;*/
+	}
+	/*StateMachine(int floor)
+	{
+		this->_current_floor = floor;
+	}*/
+
+
+	char GetData(std::ifstream& file)
+	{
+		/*char symbol;
+		symbol = file.get();*/
+		return file.get();
+	};
+	int GetCurrentFloor()
+	{
+		return this->_current_floor;
+	}
+	int GetFinalFloor()
+	{
+		return this->_final_floor;
+	}
+	bool SetCurrentFloor(int floor) {
+		if (InAlphabet(floor)) { 
+			_current_floor = floor; 
+			return true; 
+		}
+		else return false;
+	}
+	bool SetFinalFloor(int floor) {
+		if (InAlphabet(floor)) {
+			_final_floor = floor;  
+			return true;
+		}
+		else return false;
+	}
+
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	std::string fileName = "input.txt";
+	std::ifstream file;
+	file.open(fileName);
+	if (!file) { return 1; }
+
+	StateMachine Elevator;
+
+	char symbol;
+	while (0)
+	{
+		symbol = Elevator.GetData(file);
+
+	}
+
+
+
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
