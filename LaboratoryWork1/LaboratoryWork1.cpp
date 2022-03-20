67ed9b3277268df68abe0c0cfcfbb2079d789915
Dmitry Alexeev <dalexeev.work@gmail.com>
2022-03-20 16:36:45 +0300

    Greate changes

--- LaboratoryWork1/LaboratoryWork1.cpp
+++ LaboratoryWork1/LaboratoryWork1.cpp
@@ -5,6 +5,35 @@
 #include <vector>
 #include <stdlib.h>
 
+enum class State
+{
+	Start, 
+	Read,
+	Final
+};
+State current_state = State::Start;
+
+State Transition(State cur_state, char symbol)
+{
+	switch (cur_state)
+	{
+	case State::Start:
+
+		if (symbol == '\n') return State::Final;
+		
+
+		break;
+	case State::Read:
+
+		if (symbol == '\n') return State::Final;
+
+		break;
+	case State::Final:
+		return State::Final;
+		break;
+	}
+}
+
 bool InVector(std::vector<char> vect, char inputSymbol)
 {
 	auto result = std::find(begin(vect), end(vect), inputSymbol);
@@ -13,7 +42,8 @@ bool InVector(std::vector<char> vect, char inputSymbol)
 	}
 	return false;
 }
-char GetData(std::ifstream& file)
+
+char GetSymbol(std::ifstream& file)
 {
 	return file.get();
 };
@@ -26,118 +56,6 @@ char GenerateFloor(int min, int max)
 
 
 
-
-class FiniteStateMachine /*:public StateMachine*/
-{
-private:
-	std::vector<char> _alphabet{	// Входной алфавит
-		'0', '1', '2', '3', '4',
-		'5', '6', '7', '8', '9', (char)'/n', 'e'};
-	std::vector<char> _init_floors{};	// Очередь вызовов
-	std::vector<char> _final_floors{};	// Очередь конечных этажей
-	std::vector<char> _in_elevator{};	// Этажи на которые нажали люди в лифте
-	static enum class State {    // Состояния автомата
-		Start,
-		PickUp,
-		Final
-	};
-	State _current_state = State::Start;	// Текущее состояние
-	State _final_state = State::Final;		// Финальное состояние
-	int _current_floor = '0';	// Текущий этаж
-	int _final_floor = '0';		// Конечный этаж
-public:
-
-	FiniteStateMachine() {};
-
-	void Transition(char input_symbol)
-	{
-		switch (_current_state)
-		{
-		case State::Start:
-			if (input_symbol == '\n') _current_state = State::Final; 
-			if (input_symbol == 'e') _current_state = State::Final;
-			else {
-				_current_state = State::Standing;
-				_final_floor = GenerateFloor(48, 57);
-			}
-			break;
-		/*case State::Moving:
-			if (input_symbol == '\n') _current_state = State::Final;
-			break;*/
-		case State::PickUp:
-			if (input_symbol == '\n') _current_state = State::Final;
-			if (input_symbol == 'e') _current_state = State::Final;
-			else {
-				_current_state = State::Standing;
-				_current_floor = _final_floor;
-				_final_floor = GenerateFloor(48, 57);
-			}
-			break;
-		case State::Final:
-			break;
-		default:
-			break;
-		}
-	}
-	
-	std::vector<char> GetAlphabet()
-	{
-		return this->_alphabet;
-	}
-	State GetCurrentState()
-	{
-		return this->_current_state;
-	}
-	State GetFinalState()
-	{
-		return this->_final_state;
-	}
-	State GetStartState()
-	{
-		return State::Start;
-	}
-	State GetPickUpState()
-	{
-		return State::PickUp;
-	}
-	void PrintState()
-	{
-		switch (_current_state)
-		{
-		case State::Start:
-			std::cout << "Start";
-			break;
-		case State::PickUp:
-			std::cout << "Standing";
-			break;
-		case State::Final:
-		std::cout << "Final";
-			break;
-		default:
-			break;
-		}
-	}
-	void PrintCurrentFloor()
-	{
-		std::cout << _current_floor;
-	}
-	void PrintFinalFloor()
-	{
-		std::cout << _final_floor;
-	}
-};
-
-void PrintData(FiniteStateMachine Elevator)
-{
-	std::cout << "Current state ";
-	Elevator.PrintState();
-	std::cout << "\tCurrent floor ";
-	Elevator.PrintCurrentFloor();
-	std::cout << "\tFinal floor ";
-	Elevator.PrintFinalFloor();
-	std::cout << std::endl;
-};
-
 int main()
 {
 	std::string fileName = "input.txt";
@@ -146,25 +64,14 @@ int main()
 	file.open(fileName);
 	if (!file) { return 1; }
 
-	FiniteStateMachine Elevator;
+	char symbol;
 
-	//char init_floor;
-	//char final_floor;
-	char input_symbol;
-	PrintData(Elevator);
 	while (1)
 	{
-		if (Elevator.GetCurrentState() == Elevator.GetPickUpState() || Elevator.GetCurrentState() == Elevator.GetStartState())
-		{
-			input_symbol = GetData(file);
-			if (!InVector(Elevator.GetAlphabet(), input_symbol)) input_symbol = 'e';
-			Elevator.Transition(input_symbol);
-			PrintData(Elevator);
+		symbol = GetSymbol(file);
 
-		}
-		else continue;			
-		
-		if (Elevator.GetCurrentState() == Elevator.GetFinalState()) break;
+
+		if (current_state == State::Final) break;
 	}
 
 
