cfb0ce891de8a75cb0de377fba4e5ac65fadc61e
Dmitry Alexeev <DAlexeev.work@gmail.com>
2022-04-25 10:59:48 +0300

    cosmetic changes

--- lab1/Source Files/TButtons.cpp
+++ lab1/Source Files/TButtons.cpp
@@ -1,5 +1,4 @@
 #include "../Header Files/TButtons.h"
-#include "../Header Files/TState.h"
 
 std::vector <int>& TButtons::GetOuterButtons()
 {
@@ -22,15 +21,3 @@ std::vector <int>& TButtons::GetInnerButtons()
 {
     return pressedInnerButtons;
 }
-
-// bool TButtons::SetInnerButton(int place, int value)
-// {
-//     if (place < 0)
-//         return false;
-//     if (place >= FLOORS)
-//         return false;
-//     if (value != 0 and value != 1)
-//         return false;
-//     pressedInnerButtons[place] += value;
-// }
-
